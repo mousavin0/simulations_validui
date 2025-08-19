4c54ad1c596b9d263b1c52132207501defa1e2d6
Tetiana Gorbach <66789474+tetianagorbach@users.noreply.github.com>
2025-08-19 14:00:10 +0200

    Update simulation_scenarioThree.R

--- simulation_scenarioThree.R
+++ simulation_scenarioThree.R
@@ -1,19 +1,19 @@
 
 # load("scenarioThree.Rdata")
 library(devtools)
-# install("C:\\Users\\Nimo0033\\OneDrive\\PhD\\validui\\code\\packages\\ui.causal")
-# document("C:\\Users\\Nimo0033\\OneDrive\\PhD\\validui\\code\\packages\\ui.causal")
-# install("C:\\Users\\Nimo0033\\OneDrive\\PhD\\validui\\code\\packages\\ui")
-# document("C:\\Users\\Nimo0033\\OneDrive\\PhD\\validui\\code\\packages\\ui")
+# install("C:\\Users\\...\\validui\\code\\packages\\ui.causal")
+# document("C:\\Users\\...\\validui\\code\\packages\\ui.causal")
+# install("C:\\Users\\...\\validui\\code\\packages\\ui")
+# document("C:\\Users\\...\\validui\\code\\packages\\ui")
 
 
 install_github('https://github.com/stat4reg/hdim.ui.git')
 library(hdim.ui)
 
 #CHANGES AFTER REVISION
-#setwd('/home/sajjad/Neli/Rfiles_papertwo/')
+#setwd('')
 #source("validui_func.R")
-source("/Users/niloofarmoosavi/Library/CloudStorage/OneDrive-Personal/PhD/validui/code/final/Paper Two-final/validui_func.R")
+source(".../validui_func.R")
 
 
 
@@ -199,7 +199,7 @@ for(i in 1:(length(alphavec))){
 
 
 #CHANGES AFTER REVISION
-save.image("/Users/niloofarmoosavi/Library/CloudStorage/OneDrive-Personal/PhD/validui/code/final/Paper Two-final/scenarioThreeRevision.Rdata")
+save.image(".../scenarioThreeRevision.Rdata")
 
 
 
