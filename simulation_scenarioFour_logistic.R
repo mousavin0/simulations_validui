53a2330c0d480b4a704eb6252818165ef3535304
Tetiana Gorbach <66789474+tetianagorbach@users.noreply.github.com>
2025-08-19 13:57:49 +0200

    Update simulation_scenarioFour_logistic.R

--- simulation_scenarioFour_logistic.R
+++ simulation_scenarioFour_logistic.R
@@ -2,10 +2,10 @@
 # load("scenarioFour_final.Rdata")
 
 library(devtools)
-# install("C:\\Users\\Nimo0033\\OneDrive\\PhD\\validui\\code\\packages\\ui.causal")
-# document("C:\\Users\\Nimo0033\\OneDrive\\PhD\\validui\\code\\packages\\ui.causal")
-# install("C:\\Users\\Nimo0033\\OneDrive\\PhD\\validui\\code\\packages\\ui")
-# document("C:\\Users\\Nimo0033\\OneDrive\\PhD\\validui\\code\\packages\\ui")
+# install("C:\\Users\\...\\validui\\code\\packages\\ui.causal")
+# document("C:\\Users\\...\\validui\\code\\packages\\ui.causal")
+# install("C:\\Users\\...\\validui\\code\\packages\\ui")
+# document("C:\\Users\\...\\validui\\code\\packages\\ui")
 
 
 
@@ -13,9 +13,9 @@ install_github('https://github.com/stat4reg/hdim.ui.git')
 library(hdim.ui)
 
 #CHANGES AFTER REVISION
-#setwd('/home/sajjad/Neli/Rfiles_papertwo/')
+#setwd()
 #source("validui_func.R")
-source("/Users/niloofarmoosavi/Library/CloudStorage/OneDrive-Personal/PhD/validui/code/final/Paper Two-final/validui_func.R")
+source("validui_func.R")
 
 
 
@@ -214,7 +214,7 @@ for (i in 1:(length(nvec))) {
 
 
 #CHANGES AFTER REVISION
-save.image("/Users/niloofarmoosavi/Library/CloudStorage/OneDrive-Personal/PhD/validui/code/final/Paper Two-final/scenarioFourRevisionLogistic.Rdata")
+save.image(".../scenarioFourRevisionLogistic.Rdata")
 
 
 
